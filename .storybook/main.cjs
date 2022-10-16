8a80e33de803915cbf400e3f53b7dae512943a2c
Osnaelle <naellyfarias@gmail.com>
2022-10-16 15:13:37 -0300

    Add publish sotrybook workflow

--- .storybook/main.cjs
+++ .storybook/main.cjs
@@ -14,5 +14,11 @@ module.exports = {
   },
   "features": {
     "storyStoreV7": true
+  },
+  viteFinal: (config, { configType}) => {
+    if (configType ==='PRODUCTION'){
+      config.base ='/-Bootcamp-design-system'
+      }
+    return config
   }
 }
